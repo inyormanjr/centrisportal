ab776dc1c7352004222e529b4361d0f284cbc73b
inyormanjr <55531614+inyormanjr@users.noreply.github.com>
2020-03-07 23:52:06 +1100

    added global errorhandler and client intercepter

--- CentrisWebApi/Controllers/AuthController.cs
+++ CentrisWebApi/Controllers/AuthController.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using CentrisWebApi.Data;
 using CentrisWebApi.DTO;
 using CentrisWebApi.models.UserAgg;
+using CentrisWebApi.models.UserProfileAgg;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.IdentityModel.Tokens;
@@ -14,7 +15,7 @@ namespace CentrisWebApi.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
-    public class AuthController:ControllerBase
+    public class AuthController : ControllerBase
     {
 
         private readonly IAuthRepository _repo;
@@ -28,60 +29,56 @@ namespace CentrisWebApi.Controllers
         [HttpPost("Register")]
         public async Task<IActionResult> Register(UserForRegistrationDTO userForRegistrationDTO)
         {
-           
+
             userForRegistrationDTO.Username = userForRegistrationDTO.Username.ToLower();
-            
-            if(await _repo.UserExists(userForRegistrationDTO.Username))
+
+            if (await _repo.UserExists(userForRegistrationDTO.Username))
                 return BadRequest("Username already exist.");
 
-                var userToCreate = new User{
-                    username = userForRegistrationDTO.Username
-                };
+            var userToCreate = new User
+            {
+                username = userForRegistrationDTO.Username,
+                UserType = UserType.Regular
+            };
 
-                var createdUser = await _repo.Register(userToCreate, userForRegistrationDTO.Password);
-                return StatusCode(201);
+            var createdUser = await _repo.Register(userToCreate, userForRegistrationDTO.Password);
+            return StatusCode(201);
         }
 
-        [HttpPost("Login")]
+        [HttpPost("login")]
         public async Task<IActionResult> Login(UserForLoginDTO userForLoginDTO)
         {
             var userFromRepo = await _repo.Login(userForLoginDTO.Username.ToLower(), userForLoginDTO.Password);
 
-            if(userFromRepo == null)
+            if (userFromRepo == null)
+
                 return Unauthorized();
 
-                var claims = new[]
-                {
+            var claims = new[]
+            {
                     new Claim(ClaimTypes.NameIdentifier, userFromRepo.Id.ToString()),
                     new Claim(ClaimTypes.Name, userFromRepo.username)
                 };
 
-                var key = new SymmetricSecurityKey(Encoding.UTF8
-                    .GetBytes(_config.GetSection("AppSettings:Token").Value));
+            var key = new SymmetricSecurityKey(Encoding.UTF8
+                .GetBytes(_config.GetSection("AppSettings:Token").Value));
 
-                var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
 
-                var tokenDescriptor = new SecurityTokenDescriptor
-                {
-                    Subject = new ClaimsIdentity(claims),
-                    Expires = DateTime.Now.AddDays(1),
-                    SigningCredentials = creds,
-                };
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.Now.AddDays(1),
+                SigningCredentials = creds,
+            };
 
-                var tokenHandler = new JwtSecurityTokenHandler();
-                    SecurityToken token;
-                try
-                {
-                 token = tokenHandler.CreateToken(tokenDescriptor);
-                }
-                catch(Exception ex)
-                {
-                    return BadRequest(ex.Message);
-                }
-
-                return Ok(new {
-                    token = tokenHandler.WriteToken(token)
-                });
+            var tokenHandler = new JwtSecurityTokenHandler();
+            SecurityToken token;
+            token = tokenHandler.CreateToken(tokenDescriptor);
+            return Ok(new
+            {
+                token = tokenHandler.WriteToken(token)
+            });
         }
     }
 }
